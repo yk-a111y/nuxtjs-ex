c91371e3044dd3f7ad98ae888cdaf6a0cc6c4308
yk-a111y <yk19980811@163.com>
2022-05-08 23:34:38 +0100

    05

---
+++ pages/users/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>User Id</h1>
+    <p>{{ $route.params.id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserId'
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Users默认子路由</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersDefault'
+}
+</script>
+
+<style>
+
+</style>
